3401a136dac17fbd75abb8d053018930a8d0bbc3
Mhelvin Reyes <mhelvinreyes26@gmail.com>
2017-11-16 13:29:11 +0800

    fetch data from wp rest api

--- vue.js
+++ vue.js
@@ -1,24 +1,15 @@
+const req = fetch('http://localhost:8080/wp-json/wp/v2/posts').then(response => {
+  response.json().then(posts => {
+    console.log(posts,'post')
+    return posts;
+  });
+});
+
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    myRequest: req
   }
-})
+}) 
 
-var app2 = new Vue({
-    el: '#app-2',
-    data: {
-      message: 'You loaded this page on ' + new Date().toLocaleString()
-    }
-  })
-
-var app4 = new Vue({
-el: '#app-4',
-data: {
-    todos: [
-    { text: 'Learn JavaScript' },
-    { text: 'Learn Vue' },
-    { text: 'Build something awesome' }
-    ]
-}
-})
+console.log(req.resolve(),'req');
